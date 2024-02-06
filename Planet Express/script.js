763ea1180baf77f22abe3f3f23da60017b5fc9b4
PatoLubricado <m.jorquera@live.com>
2024-02-06 19:12:51 +0100

    Updated some files

--- Planet Express/script.js
+++ Planet Express/script.js
@@ -8,18 +8,19 @@ document.getElementById(shown).style.display='block';
 document.getElementById(hidden1).style.display='none';
 document.getElementById(hidden2).style.display='none';
 document.getElementById(hidden3).style.display='none';
+scrolltop();
 }
 
 function scrolltop() {window.scrollTo(0, 0);}
-
-function changeFont() {
-  var selectedFont = document.getElementById('lang').value;
-  document.querySelectorAll('html, input, select').forEach(function (element) {
-    element.style.fontFamily = selectedFont;
-  });
-}
 function updatePaddingTop() {
   document.body.style.paddingTop = 1.1 * document.getElementById('nav-bar').clientHeight + 'px';
+}
+
+function changeFont() {
+  var newFont = document.getElementById('lang').value;
+  document.querySelectorAll('html, input, select').forEach(function (element) {
+    element.style.fontFamily = newFont;
+  });
 }
 
 updatePaddingTop();
